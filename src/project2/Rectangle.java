b8498f14036ee305f6a1fe5b61b8fbf7fd8d9002
MasyudiPutra <32509043+MasyudiPutra@users.noreply.github.com>
2018-03-27 13:02:18 +0800

    Membuat atribut dan behavior

--- src/project2/Rectangle.java
+++ src/project2/Rectangle.java
@@ -1,4 +1,13 @@
 package project2;
 public class Rectangle {
+    double panjang;
+    double lebar;
     
+    void cetakinfo(){
+        
+        System.out.println("====================");
+        System.out.println("panjang = "+panjang);
+        System.out.println("lebar = "+lebar);
+        System.out.println("====================");
+    }
 }
